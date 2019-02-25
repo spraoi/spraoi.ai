c454db517008e4bae1f8a790101f47cc1cc77449
Cade Scroggins <git@cadejs.com>
2019-02-25 11:43:15 -0800

    explicit defer attribute

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -129,7 +129,7 @@ const SEO = ({ article, pathname, ...overrides }) => (
                 name: 'Breadcrumbs',
               })}
             </script>
-            <script defer src="/js/mtiFontTrackingCode.js" />
+            <script defer="true" src="/js/mtiFontTrackingCode.js" />
           </Helmet>
           <Facebook
             desc={data.description}

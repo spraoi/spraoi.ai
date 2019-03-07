5a9e5913782dde3d2eada44d9fd3accc58e8df3d
Cade Scroggins <git@cadejs.com>
2019-03-07 11:38:20 -0800

    add article share buttons

--- src/components/ArticleLayout/index.js
+++ src/components/ArticleLayout/index.js
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { Facebook, Linkedin, Mail, Twitter } from 'react-social-sharing';
 import ArticlesContainer from '../../containers/ArticlesContainer';
+import MetadataContainer from '../../containers/MetadataContainer';
 import SEO from '../SEO';
 import { HeroSection, Section, SectionH1, StyledImage } from '../Section';
 import 'prism-themes/themes/prism-vs.css';
@@ -65,8 +67,8 @@ const ArticleSection = styled(Section)`
 
   blockquote {
     max-width: ${p => p.theme.maxWidths.md};
-    margin: 0 -${p => p.theme.space.md} ${p => p.theme.space.lg};
-    padding: ${p => p.theme.space.md} ${p => p.theme.space.lg};
+    margin: 0 0 ${p => p.theme.space.lg};
+    padding: ${p => p.theme.space.md};
     border-left: solid ${p => p.theme.space.sm} ${p => p.theme.colors.border};
     background-color: ${p => p.theme.colors.white};
 
@@ -76,7 +78,12 @@ const ArticleSection = styled(Section)`
 
     @media (min-width: ${p => p.theme.breakpoints.sm}) {
       margin: 0 0 ${p => p.theme.space.lg};
+      padding: ${p => p.theme.space.md} ${p => p.theme.space.lg};
     }
+  }
+
+  pre {
+    border: solid 1px ${p => p.theme.colors.border};
   }
 
   .gatsby-highlight {
@@ -84,48 +91,109 @@ const ArticleSection = styled(Section)`
   }
 `;
 
-const ArticleLayout = ({ location: { pathname }, pageContext: { slug } }) => (
-  <ArticlesContainer>
-    {articles => {
-      const { author, frontmatter, html } = articles.find(
-        article => article.fields.slug === slug
-      );
+const ArticleFooter = styled.footer`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: ${p => p.theme.space.xxl};
 
-      return (
-        <article>
-          <SEO
-            article={frontmatter}
-            banner={frontmatter.banner}
-            dateModified={frontmatter.dateModified}
-            datePublished={frontmatter.datePublished}
-            description={frontmatter.description}
-            pathname={pathname}
-            person={author}
-            title={frontmatter.title}
-          />
-          <HeroSection single>
-            <SectionH1 wrap={1}>{frontmatter.title}</SectionH1>
-            <Details>
-              <Link to={`/people/${author.id}`}>
-                <PersonImage clickable>
-                  <Img alt="" fluid={author.image} />
-                </PersonImage>
-              </Link>
-              <ul>
-                <li>
-                  <PersonName to={`/people/${author.id}`}>
-                    {author.givenName} {author.familyName}
-                  </PersonName>
-                </li>
-                <li>{frontmatter.datePublished}</li>
-              </ul>
-            </Details>
-          </HeroSection>
-          <ArticleSection dangerouslySetInnerHTML={{ __html: html }} single />
-        </article>
-      );
-    }}
-  </ArticlesContainer>
+  a {
+    margin: ${p => p.theme.space.md} ${p => p.theme.space.md} 0 0;
+
+    & > div {
+      padding: ${p => p.theme.space.sm} ${p => p.theme.space.md};
+      border-radius: ${p => p.theme.radii.md};
+      box-shadow: ${p => p.theme.boxShadows.md};
+      transition: background-color ${p => p.theme.transitionSpeeds.normal};
+    }
+
+    span {
+      margin-left: ${p => p.theme.space.xxxs};
+      font-family: ${p => p.theme.fonts.primary};
+      font-size: ${p => p.theme.fontSizes.sm};
+      font-weight: ${p => p.theme.fontWeights.semibold};
+      letter-spacing: ${p => p.theme.letterSpacings.sm};
+      text-transform: uppercase;
+    }
+  }
+`;
+
+const ArticleLayout = ({ location: { pathname }, pageContext: { slug } }) => (
+  <MetadataContainer>
+    {({ siteUrl }) => (
+      <ArticlesContainer>
+        {articles => {
+          const { author, frontmatter, html } = articles.find(
+            article => article.fields.slug === slug
+          );
+
+          const shareUrl = `${siteUrl}/articles${slug}`;
+
+          return (
+            <article>
+              <SEO
+                article={frontmatter}
+                banner={frontmatter.banner}
+                dateModified={frontmatter.dateModified}
+                datePublished={frontmatter.datePublished}
+                description={frontmatter.description}
+                pathname={pathname}
+                person={author}
+                title={frontmatter.title}
+              />
+              <HeroSection single>
+                <SectionH1 wrap={1}>{frontmatter.title}</SectionH1>
+                <Details>
+                  <Link to={`/people/${author.id}`}>
+                    <PersonImage clickable>
+                      <Img alt="" fluid={author.image} />
+                    </PersonImage>
+                  </Link>
+                  <ul>
+                    <li>
+                      <PersonName to={`/people/${author.id}`}>
+                        {author.givenName} {author.familyName}
+                      </PersonName>
+                    </li>
+                    <li>{frontmatter.datePublished}</li>
+                  </ul>
+                </Details>
+              </HeroSection>
+              <ArticleSection
+                dangerouslySetInnerHTML={{ __html: html }}
+                single
+              />
+              <ArticleFooter>
+                <Twitter
+                  big
+                  link={shareUrl}
+                  message={frontmatter.title}
+                  solidcircle
+                />
+                <Facebook
+                  big
+                  link={shareUrl}
+                  message={frontmatter.title}
+                  solidcircle
+                />
+                <Linkedin
+                  big
+                  link={shareUrl}
+                  message={frontmatter.title}
+                  solidcircle
+                />
+                <Mail
+                  big
+                  link={shareUrl}
+                  message={frontmatter.title}
+                  solidcircle
+                />
+              </ArticleFooter>
+            </article>
+          );
+        }}
+      </ArticlesContainer>
+    )}
+  </MetadataContainer>
 );
 
 ArticleLayout.propTypes = {

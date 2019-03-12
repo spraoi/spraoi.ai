0eb4e4f514b4bdc2d416c0ae4a95b036b1bba470
Cade Scroggins <git@cadejs.com>
2019-03-11 18:35:56 -0700

    insta has www

--- src/components/SEO/helpers.js
+++ src/components/SEO/helpers.js
@@ -3,7 +3,7 @@ export const getSiteTitle = (pathname, siteName, title) =>
 
 export const getSocialUrls = ({ instagram, twitter, facebook, linkedIn }) => {
   const socialUrls = [];
-  if (instagram) socialUrls.push(`https://instagram.com/${instagram}`);
+  if (instagram) socialUrls.push(`https://www.instagram.com/${instagram}`);
   if (twitter) socialUrls.push(`https://twitter.com/${twitter}`);
   if (facebook) socialUrls.push(`https://www.facebook.com/${facebook}`);
   if (linkedIn) socialUrls.push(`https://www.linkedin.com/company/${linkedIn}`);
